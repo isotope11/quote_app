2d478401e09dab9461ba5976d580db230286482e
johndavid400 <johndavid400@yahoo.com>
2012-01-06 16:08:57 -0600

    [NEW QUOTE] added some pages and stuff

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,4 +6,15 @@ class QuotesController < ApplicationController
   def show
     @quote = Quote.find(params[:id])
   end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(params[:quote])
+    if @quote.save
+      redirect_to @quote
+    end
+  end
 end

7f7bd615300cde553c37bc316e8cf36123901be1
Ben Holley <ben@isotope11.com>
2012-01-09 16:16:51 -0600

    add integration tests for quotes

--- test/integration/quote_integration_test.rb
+++ test/integration/quote_integration_test.rb
@@ -2,8 +2,8 @@ require_relative '../test_helper'
 
 describe "Quote integration" do
   before do
-    @quote = Factory :quote
-    @section = Factory :section, parent: @quote
+    @quote = FactoryGirl.create :quote
+    @section = FactoryGirl.create :section, parent: @quote
   end
 
   it "should show the quote" do
@@ -14,6 +14,32 @@ describe "Quote integration" do
   it "should show sub-sections" do
     visit quote_path(@quote)
     page.must_have_content @section.description
+  end
+
+  it "has a workflow for creating toplevel sections" do
+    visit quote_path @quote
+    within 'h3' do
+      click_link 'edit'
+    end
+    click_link 'New Section'
+    fill_in 'Description', with: 'Engineering'
+    click_button 'Create Section'
+    current_path.must_equal quote_path @quote
+    page.must_have_content 'Engineering'
+  end
+
+  it "has a workflow for creating items" do
+    visit quote_path @quote
+    within '.section' do
+      click_link 'edit'
+    end
+    click_link 'New Child Item'
+    fill_in 'Description', with: 'application modeling'
+    fill_in 'Min hours', with: '4'
+    fill_in 'Max hours', with: '8'
+    click_button 'Create Item'
+    current_path.must_equal quote_path @quote
+    page.must_have_content 'application modeling'
   end
 end
 

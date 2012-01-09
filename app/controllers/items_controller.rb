840ab073b5976e1efc6efa785b89d129ff1ab5c6
Ben Holley <ben@isotope11.com>
2012-01-09 09:18:17 -0600

    add simple edit functionality to sections and items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,14 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find params[:id]
   end
+
+  def update
+    @item = Item.find params[:id]
+    if @item.update_attributes params[:item]
+      redirect_to quote_path @item.parent.parent
+    else
+      flash.notice = 'There were some errors.'
+      render :edit
+    end
+  end
 end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,4 +2,14 @@ class SectionsController < ApplicationController
   def edit
     @section = Section.find params[:id]
   end
+
+  def update
+    @section = Section.find params[:id]
+    if @section.update_attributes params[:section]
+      redirect_to quote_path @section.parent
+    else
+      flash.alert = 'There were some errors.'
+      render :edit
+    end
+  end
 end

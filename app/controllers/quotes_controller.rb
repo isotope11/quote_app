216aef57397a1b58f82434792036d57a5b4ab938
Ben Holley <ben@isotope11.com>
2012-01-09 09:55:12 -0600

    add edit page for a quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -17,4 +17,18 @@ class QuotesController < ApplicationController
       redirect_to @quote
     end
   end
+
+  def edit
+    @quote = Quote.find params[:id]
+  end
+
+  def update
+    @quote = Quote.find params[:id]
+    if @quote.update_attributes params[:quote]
+      redirect_to @quote
+    else
+      flash.notice = 'There were some errors.'
+      render :edit
+    end
+  end
 end

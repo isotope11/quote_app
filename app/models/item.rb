61340a317847251370ee6f8e7500d118b7d2e3e0
Ben Holley <ben@isotope11.com>
2012-02-03 10:14:24 -0600

    validate parent on Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < Node
   validate :parent, :parent_must_be_section
-  validates_presence_of :min_hours, :max_hours
+  validates_presence_of :min_hours, :max_hours, :parent
 
   belongs_to :item_template
 
   def parent_must_be_section
-    if parent.type != 'Section'
+    if parent && parent.type != 'Section'
       errors.add :parent, 'must be a section'
     end
   end

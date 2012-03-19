d5433a629fbafd0824e89846e27c07ed4af031f0
Ben Holley <ben@isotope11.com>
2012-03-19 09:01:26 -0500

    update assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ QuoteApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -44,6 +44,7 @@ QuoteApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['print.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
